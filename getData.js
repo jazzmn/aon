961cac3eb6c6b8f95fb56c342cbb2146e0f8aaae
Tobias Ceska <tobias.ceska@realtech.com>
2018-09-12 17:33:26 +0200

    kpi upload ergaenzt

--- getData.js
+++ getData.js
@@ -4,7 +4,7 @@ const _=require('lodash');
 const log4js = require("log4js"), log4js_extend = require("log4js-extend");
 const os=require('os');
 const requestLib = require('request')
-const azure = require('azure-storage');
+const neatCsv = require('neat-csv');
 const moment = require('moment');
 moment.locale('de')
 
@@ -34,67 +34,63 @@ const reqP = (reqOpt)=>{
 
 (async () => {
 	
-	logger.debug("start login")
+	logger.info("login to TA-Box: "+config.taBox.baseUrl)
 	await reqP({ method: 'POST',
 	  url: config.taBox.baseUrl+'/start.html',
 	  headers: 
-	   { 'cache-control': 'no-cache',
-		 'content-type': 'application/x-www-form-urlencoded' },
+	   { 'content-type': 'application/x-www-form-urlencoded' },
 	  form: 
 	   { username: config.taBox.username,
 		 password: config.taBox.password }
 	})
 	
-	logger.debug("get csvData")
+	logger.info("get csvData from TA-Box")
 	let csvData=await reqP({ method: 'GET',
 	  url: config.taBox.baseUrl+'/cgi-bin/getCSV',
 	  qs: 
-	   { ts_from: 15012938012 }
+	   { ts_from: 15012938012 } //TODO: params vervollstaendigen
 	})
 	logger.debug(csvData)
 	
-	logger.debug("start uploading to storage")
-	let azureFileService = (config.azure.connectionString) ? 
-		azure.createFileService(config.azure.connectionString) : 
-		azure.createFileService(config.azure.accountName, config.azure.accountKey)
-	, shareName=config.azure.shareName
-	, dirName=config.azure.dirName
-	, filename=moment().format('YYYY-MM-DD')
+	logger.info("login to dot4SaKpiRepository: "+config.dot4SaKpiRepository.baseUrl)
+	let kpiRepLogin=await reqP({ method: 'POST',
+	  url: config.dot4SaKpiRepository.baseUrl+'/token',
+	  json: true,
+	  form: 
+	   { 'apiKey': config.dot4SaKpiRepository.apiKey }
+	})
+	, kpiRepToken=kpiRepLogin['x-api-key']
 	
-	//lege share an, falls nicht existiert
-	await new Promise((resolve, reject)=> {
-		azureFileService.createShareIfNotExists(shareName, function(error, result, response) {
-		  if (error) 
-			return reject(error);
-		  
-		  logger.debug("azure storage ok: "+shareName)
-		  resolve();
-		});
-	});
+	logger.info("get Dot4 service IDs from dot4SaKpiRepository")
+	let allServices=await reqP({ method: 'GET',
+	  url: config.dot4SaKpiRepository.baseUrl+'/service',
+	  headers: { 'x-api-key': kpiRepToken },
+	  json: true,
+	  form: 
+	   { 'apiKey': config.dot4SaKpiRepository.apiKey }
+	})
+	, serviceName="Printing FollowMe"
 	
-	//lege directory an, falls nicht existiert
-	await new Promise((resolve, reject)=> {
-		azureFileService.createDirectoryIfNotExists(shareName, dirName, function(error, result, response) {
-		  if (error) 
-			return reject(error);
-		  
-		  logger.debug("azure directory ok: "+dirName)
-		  resolve();
-		});
-	});
+	//TODO:
+	// woher kommt der service name
+	, serviceUid=_.get( _.find(allServices.data, { name: serviceName }), 'uid')
 	
-	//Upload to Storage
-	await new Promise((resolve, reject)=> {
-	azureFileService.createFileFromText(shareName, dirName, filename, csvData, function(error, result, response) {
-		  if (error) 
-			return reject(error);
-		  
-		  logger.debug("copy to azure storage succeeded: "+filename)
-		  resolve();
-		});
-	});
-					
-				
+	//TODO:
+	// calculate kpi
+	let data=await neatCsv(csvData)
+	let calculatedKpi={
+		serviceUid
+		, value: 1
+	}
+	
+	logger.info("upload new kpi values into dot4SaKpiRepository")
+	await reqP({ method: 'POST',
+	  url: config.dot4SaKpiRepository.baseUrl+'/service/customkpi',
+	  headers: { 'x-api-key': kpiRepToken },
+	  form: 
+	   { serviceUid: calculatedKpi.serviceUid,
+		 paperCountBW: calculatedKpi.value } //TODO: woher kommt dieser Key
+	})
 })().catch(e => {
    logger.error(e)
 });
